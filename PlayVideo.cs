034f5d93ec0a1a6241ccab9e663e1dc940999a96
Felix_J <111042013+DevFelixJ@users.noreply.github.com>
2022-11-21 09:37:49 +0100

    Update PlayVideo.cs
    Implementado una corutina para que al cabo de 15 segundos, los gameObjects desactivados durante la reproduccion de video, se activen.

--- PlayVideo.cs
+++ PlayVideo.cs
@@ -6,10 +6,15 @@ public class PlayVideo : MonoBehaviour
 {
     public GameObject videoPlayer;
     public int timeToStop;
+    public GameObject backGround;
+    public GameObject player;
+    public GameObject plane;
+    public GameObject drag;
     // Start is called before the first frame update
     void Start()
     {
         videoPlayer.SetActive(false);
+        backGround.SetActive(false);
         
     }
 
@@ -18,8 +23,25 @@ public class PlayVideo : MonoBehaviour
         if (apple.gameObject.tag == "Apple")
         {
             videoPlayer.SetActive(true);
+            backGround.SetActive(true);
+            player.SetActive(false);
+            plane.SetActive(false);
+            drag.SetActive(false);
             Destroy(videoPlayer, timeToStop);
+            StartCoroutine(Restore());
+            
         }
-        
+        IEnumerator Restore()
+        {
+            yield return new WaitForSeconds(15);
+            player.SetActive(true);
+            plane.SetActive(true);
+            drag.SetActive(true);
+            backGround.SetActive(false);
+
+        }
+
+
     }
+    
 }
